0ad29cb3a06b13dafd46b9692acea5c2a2e4a5d3
namtran232 <namphongtran232@gmail.com>
2023-06-01 16:31:38 +0700

    Update type useDispatch, useSelector

--- src/redux/store.tsx
+++ src/redux/store.tsx
@@ -35,6 +35,6 @@ const store = configureStore({
     }),
 });
 export type RootState = ReturnType<typeof store.getState>;
-export type AppDispatch = typeof store.dispatch;
+export type AppDispatch = typeof store.dispatch
 export const persistor = persistStore(store);
 export default store;
