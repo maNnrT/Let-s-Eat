c6f621999dcfe18b5faa1fb3fe201f3838bddbf0
tran phong nam <namphongtran232@gmail.com>
2023-08-14 22:53:06 +0700

    Delete unused API URL

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const request = axios.create({
-  // baseURL: 'http://localhost:3000',
   baseURL: import.meta.env.VITE_API_BASE_URL,
-  // baseURL: 'https://let-s-eat-server-2.vercel.app',
+
+
 });
 export const get = async (apiPath: string, options = {}) => {
   const respone = await request.get(apiPath, options);
